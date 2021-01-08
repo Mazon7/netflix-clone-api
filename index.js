56e09ad9150f93b53f4f071b082dcecde4cbda39
Max <maximgusho7@gmail.com>
2021-01-08 14:27:48 +0300

    Added JWT functionality

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express')  // import express
 const app = express() // initialize express
 const port = 3000 // setting the port
 const mongoose = require('mongoose');
+const jwt = require("jsonwebtoken"); // configure JWT
 const { Schema } = mongoose; // Grab the schema object from mongoose
 var cors = require('cors'); // configure CORS policy
 require('dotenv').config();
+
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.89oxm.mongodb.net/netflix-dev?retryWrites=true&w=majority`,
 {
@@ -32,11 +34,36 @@ const User = mongoose.model('Users', new Schema(
 app.use(cors()); // using CORS
 app.use(express.json());
 
+function authenticateToken (req, res, next) {
+  console.log(req.headers);
+  const authHeaderToken = req.headers['authorization']
+  if (!authHeaderToken) return res.sendStatus(401);
+
+  jwt.verify(authHeaderToken, "dsfs49skk4eoisa", (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next()
+  })
+}
+
 // using the get method
 // LOGIC for the Get Request
 // I'm trying to get data
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/wishlist', authenticateToken, (req, res) => {
+  console.log('I am authenticated');
+  console.log(req.user);
+  WishList.findOne({user: user.id})
+  res.send({
+    items: [
+      "The Avengers",
+      "Tenet",
+      "Queens Gambit"
+    ]
+  })
 })
 
 app.post('/register', (req, res) => {
@@ -61,15 +88,27 @@ app.post('/register', (req, res) => {
 })
 
 
+function generateAccessToken (user) {
+  const payload = {
+    id: user.id,
+    name: user.name
+  }
+  return jwt.sign(payload, "dsfs49skk4eoisa", {expiresIn: '7200s'});
+}
+
 app.post('/login', (req, res) => {
   const password  = req.body.password;
   const email = req.body.email;
   User.findOne({ email: email, password: password }, (err, user) => {
     console.log(user);
     if (user) {
+      console.log(user);
+      const token =  generateAccessToken(user);
+      console.log(token);
+
       res.send ({
         status: "valid",
-        token: user.id
+        token: token
       });
     }  else {
       res.status(404, {
